4103c3c5fcfdc36bc2c88105ecdd65413ec76608
Kai <112913093+PSU-Kai@users.noreply.github.com>
2023-08-29 13:34:52 -0700

    Add files via upload

---
+++ dcm_client.py
@@ -0,0 +1,62 @@
+import os
+import requests
+import csv
+
+# Define the path to the CSV file
+csv_file_path = '/home/pi/client/data.csv'
+
+# Function to read the last row of CSV and extract values
+def read_last_row(csv_file_path):
+    try:
+        with open(csv_file_path, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            rows = list(csv_reader)
+            if len(rows) >= 2:  # Check if there are at least 2 rows (header + data)
+                last_row = rows[-1]  # Get the last row
+                energy_take = float(last_row[0])  # Extract energy_take from the last row
+                duration = float(last_row[1])  # Extract duration from the last row
+                power = float(last_row[2])  # Extract power from the last row
+                interval = float(last_row[3])  # Extract interval from the last row
+                return interval, duration, power, energy_take
+            else:
+                print(f"Not enough rows in {csv_file_path} to extract data.")
+    except FileNotFoundError:
+        print(f"CSV file not found at {csv_file_path}")
+    except Exception as e:
+        print(f"Error reading CSV: {str(e)}")
+    return None, None, None, None
+
+path = os.getcwd()
+c_cert_file_path = path + '/cert/client.crt'
+c_key_file_path = path + '/cert/client.key'
+certServer = path + '/cert/derms.crt'
+
+host_name = 'psupwrlabderms.ddns.net'
+host_port = 443
+host_address = f'https://{host_name}:{host_port}'
+
+# Read values from the last row of CSV
+interval, duration, power, energy_take = read_last_row(csv_file_path)
+
+if interval is not None:
+    xml = f"""<?xml version="1.0" encoding="UTF-8"?>
+    <Order>
+      <Id>78912</Id>
+      <to>gsp</to>
+      <from>client</from>
+      <Customer>SPC_4</Customer>
+      <message>Simple Test Message</message>
+      <Interval>{interval}</Interval>
+      <Duration>{duration}</Duration>
+      <Power>{power}</Power>
+      <EnergyTake>{energy_take}</EnergyTake>
+    </Order>"""
+
+    headers = {'Content-Type': 'application/xml'}
+
+    r = requests.post(host_address, data=xml, verify=certServer, headers=headers, cert=(c_cert_file_path, c_key_file_path))
+    print(r.status_code)
+else:
+    print("CSV values not available, request not sent.")
+
+
